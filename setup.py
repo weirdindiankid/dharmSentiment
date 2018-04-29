4deedc281c85db488c4b6a68cfb307ca36064b75
Dharmesh Tarapore <dharmesh@bu.edu>
2018-04-29 16:34:10 -0400

    Fixes

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author = 'Dharmesh Tarapore',
     author_email = 'dharmesh@cs.bu.edu',
     url = 'https://github.com/weirdindiankid/dharmSentiment', 
-    download_url = 'https://github.com/weirdindiankid/dharmSentiment/archive/v2.8.4.tar.gz',
+    download_url = 'https://github.com/weirdindiankid/dharmSentiment/archive/v1.0.0.tar.gz',
     keywords = [],
     classifiers = ['Development Status :: 4 - Beta',
                    'Programming Language :: Python'],
