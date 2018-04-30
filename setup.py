69bb70c432d7749f3a05828308caf6375f9e5b56
Dharmesh Tarapore <dharmesh@bu.edu>
2018-04-30 00:30:27 -0400

    ARF

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     'data/warriner/*.csv','data/ANEW/all*.csv',
     'data/MPQA-lexicon/subjectivity_clues_hltemnlp05/*.tff',
     'data/liu-lexicon/*-clean.txt','static/*.js','static/hedotools.shift.css']},
-    version = '3.0.1',
+    version = '3.0.2',
     description = 'Basic usage script for dictionary-based sentiment analysis.\
     Intended use with labMT data. Modified for Dharmesh\'s personal use in CS 591 L1.',
     long_description = long_description,
