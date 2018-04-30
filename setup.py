b777e9da4847507c07d3db8a335457cb88b2dba5
Dharmesh Tarapore <dharmesh@bu.edu>
2018-04-30 00:32:43 -0400

    Add

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     packages = ['dharmSentiment'],
     package_data={'dharmSentiment': [
     'data/labMT/*.txt','data/PANAS-X/*.txt',
-    'data/warriner/*.csv','data/ANEW/all*.csv',
+    'data/warriner/*.csv','data/ANEW/all*.csv', 'README.markdown', 'LICENSE.markdown',
     'data/MPQA-lexicon/subjectivity_clues_hltemnlp05/*.tff',
     'data/liu-lexicon/*-clean.txt','static/*.js','static/hedotools.shift.css']},
-    version = '3.0.2',
+    version = '3.0.3',
     description = 'Basic usage script for dictionary-based sentiment analysis.\
     Intended use with labMT data. Modified for Dharmesh\'s personal use in CS 591 L1.',
     long_description = long_description,
